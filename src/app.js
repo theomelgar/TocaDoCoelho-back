4262dfebfc04beb9668c93349ecdcab21860a02d
Andre <andre.eng1313@gmail.com>
2023-01-25 16:16:40 -0300

    teste commit

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 const PORT = process.env.PORT
 
-//routes
+//routes teste andre
 app.use([ authRouter, operationsRouter ])
 
 app.listen(PORT, () => console.log("Servidor online na porta: " + PORT)) 
