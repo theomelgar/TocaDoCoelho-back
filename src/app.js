d3eff81f34630ef80e0f8a91b878f404dfc4a0aa
theomelgar <theomelgar@gmail.com>
2023-01-30 03:05:10 -0300

    fix: adicionei do router no app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import authRouter from "./routes/AuthRoutes.js"
 import operationsRouter from "./routes/ProdutosRoutes.js"
+import FinalizarRouter from "./routes/FinalizarRoutes.js"
 
 const app = express()
 app.use(express.json())
@@ -10,6 +11,6 @@ app.use(cors())
 const PORT = process.env.PORT
 
 //routes
-app.use([ authRouter, operationsRouter ])
+app.use([ authRouter, operationsRouter, FinalizarRouter ])
 
 app.listen(PORT, () => console.log("Servidor online na porta: " + PORT)) 
--- src/controller/Produtos.js
+++ src/controller/Produtos.js
@@ -23,22 +23,3 @@ export async function idProduto(req, res) {
         res.status(500).send((error))
     }
 }
-
-export async function finalizarCompra(req, res){
-
-    const compra = req.body;
-
-    try{
-
-        await db.collection("compras").insertOne(compra);
-
-        res.status(201).send("Compra finalizada com sucesso!");
-
-    } catch (error){
-
-        console.log(error);
-
-        res.status(500).send("Erro no servidor");
-
-    }
-}
