b44936f24e91cd215f5b2c779eb5bbbda5defd79
theomelgar <theomelgar@gmail.com>
2023-01-30 03:05:10 -0300

    teste

--- src/schemas/FinalizarCompraSchema.js
+++ src/schemas/FinalizarCompraSchema.js
@@ -5,10 +5,10 @@ import joi from "joi";
 export const finalizarCompraSchema = joi.object({
     nome: joi.string().required(),
     email: joi.string().email().required(),
-    cep: joi.number().required(),
+    cep: joi.string().required(),
     formaDePagamento: joi.string().required(),
     cesta: joi.array().required(),
-    total: joi.number().required(),
+    total: joi.string().required(),
     idUsuario: joi.string().required(),
     criadoEm: joi.string().required()
 });
