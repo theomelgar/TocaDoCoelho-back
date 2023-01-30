37f639c2ba491bb98adf0662d4a33ed051a05875
theomelgar <theomelgar@gmail.com>
2023-01-30 16:49:46 -0300

    feat: sendgrid email enviado

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb"
+import sgMail from '@sendgrid/mail'
 
 dotenv.config()
 
 let db;
 
 const mongoClient = new MongoClient(process.env.MONGO_URI)
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 try {
     await mongoClient.connect()
     db = mongoClient.db("Coelho")
--- src/controller/Finalizar.js
+++ src/controller/Finalizar.js
@@ -1,22 +1,31 @@
 import db from "../config/database.js";
+import sgMail from '@sendgrid/mail'
 
 //CONTROLLER
 
-export async function finalizarCompra(req, res){
+export async function finalizarCompra(req, res) {
+  const compra = req.body;
 
-    const compra = req.body;
+  try {
 
-    try{
-
-        await db.collection("compras").insertOne(compra);
-
-        res.status(201).send("Compra finalizada com sucesso!");
-
-    } catch (error){
-
-        console.log(error);
-
-        res.status(500).send("Erro no servidor");
-
+    await db.collection("compras").insertOne(compra);
+    const msg = {
+      to: compra.email, // Change to your recipient
+      from: 'tocadocoelhocompra@gmail.com', // Change to your verified sender
+      subject: 'Compra efetuada '+ compra.nome,
+      text: 'Obrigado por comprar conosco, seus produtos já estão sendo preparados para o envio.',
+      html: '<h1><strong>Obrigado por comprar conosco, seus produtos já estão sendo preparados para o envio.</strong></h1><br><br><h1>Suas compras: '+compra.cesta.map((c)=>c.produto)+'</h1><br><br><h1>Total: '+compra.total+'</h1>',
     }
+
+    await sgMail.send(msg);
+
+    res.status(201).send("Compra finalizada com sucesso!");
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).send("Erro no servidor");
+
+  }
 }
